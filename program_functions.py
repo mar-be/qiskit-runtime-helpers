28e9deccbe4e258a5f0900ab093212c20b592028
Marvin Bechtold <marvin.bechtold.dev@gmail.com>
2022-03-10 12:10:53 +0100

    Remove callback function

--- program_functions.py
+++ program_functions.py
@@ -1,21 +1,6 @@
 import logging
-import time
 
 from runtime_helpers.user_messenger_logging import UserMessengerHandler
-
-
-def get_callback(user_messenger):
-    callback_counter = 0
-    callback_time = time.time()
-
-    def _callback(x):
-        nonlocal callback_counter
-        nonlocal callback_time
-        user_messenger.publish({'iteration': callback_counter, 'duration': time.time() - callback_time, 'params': x})
-        callback_time = time.time()
-        callback_counter += 1
-
-    return _callback
 
 
 def activate_logging(user_messenger, log_level, log_modules):
